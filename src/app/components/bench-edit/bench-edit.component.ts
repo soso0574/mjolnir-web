08c2234cfa60a6624d8b3aedc48fc5a10dcea6f0
srajas <srajass92@gmail.com>
2018-02-09 00:10:24 +0530

    Employer Profile Edit Mode

--- src/app/components/bench-edit/bench-edit.component.ts
+++ src/app/components/bench-edit/bench-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { DragulaService } from 'ng2-dragula';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
@@ -7,27 +7,45 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './bench-edit.component.html',
   styleUrls: ['./bench-edit.component.scss']
 })
-export class BenchEditComponent implements OnInit, OnDestroy {
+export class BenchEditComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() data: any;
   @Output() onBack = new EventEmitter();
   today = new Date();
   dates: any;
   times: any;
+  bags = 'bag-one';
+  options = {
+    revertOnSpill: true,
+    direction: 'horizontal',
+  };
 
   constructor(
     private modalService: NgbModal,
     private dragulaService: DragulaService,
   ) {
-    dragulaService.setOptions('bag-one', {
-      revertOnSpill: true,
-      direction: 'horizontal',
-    });
+    this.dragulaService.setOptions('bag-one', this.options);
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.dragulaService.destroy('bag-one'); // destroy dragula bag
+  }
 
   ngOnDestroy() {
     this.dragulaService.destroy('bag-one'); // destroy dragula bag
+  }
+
+  // Enable Dragg and Drop
+  editing() {
+    if (this.bags.length > 0) {
+      this.dragulaService.setOptions('bag-one', this.options);
+      this.bags = '';
+    } else {
+      this.bags = 'bag-one';
+      this.dragulaService.destroy('bag-one'); // destroy dragula bag
+    }
   }
 
   // Send $eent to parent component
